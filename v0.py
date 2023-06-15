3ad45883dd19273c2d0aabfb21eb3f3ee5b8c89d
net1ife <127271713+net1ife@users.noreply.github.com>
2023-06-14 23:43:11 -0400

    Update v0.py

--- v0.py
+++ v0.py
@@ -1,92 +1,65 @@
 import numpy as np
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader, TensorDataset
+import mne
+from sklearn import svm
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
-# Generate synthetic data
-np.random.seed(0)
-n_samples = 1000
-n_channels = 32  # number of EEG channels
-n_timepoints = 100  # number of timepoints per sample
-X = np.random.normal(0, 1, (n_samples, n_channels, n_timepoints))
-y = np.random.choice([0, 1], n_samples)
+# Signal Acquisition
+def acquire_signal(file):
+    # Load the EEG data from a file
+    raw = mne.io.read_raw_fif(file, preload=True)
+    return raw
 
-# Split data into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Signal Preprocessing
+def preprocess_signal(raw):
+    # Apply bandpass filtering
+    raw.filter(l_freq=1, h_freq=40)
 
-# Normalize data
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
-X_test = scaler.transform(X_test.reshape(-1, X_test.shape[-1])).reshape(X_test.shape)
+    # Remove EOG artifacts
+    eog_events = mne.preprocessing.find_eog_events(raw)
+    n_bads, scores = mne.preprocessing.ica_find_bads_eog(raw, eog_events)
+    raw = mne.preprocessing.ICA(n_components=0.95).fit(raw).apply(raw, exclude=n_bads)
 
-# Convert data to PyTorch tensors
-X_train, y_train = torch.tensor(X_train).float(), torch.tensor(y_train).long()
-X_test, y_test = torch.tensor(X_test).float(), torch.tensor(y_test).long()
+    return raw
 
-# Create PyTorch dataloaders
-train_data = TensorDataset(X_train, y_train)
-test_data = TensorDataset(X_test, y_test)
-batch_size = 32
-train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
-test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
+# Feature Extraction
+def extract_features(raw):
+    # Compute the power spectral density
+    psds, freqs = mne.time_frequency.psd_multitaper(raw, fmin=1, fmax=40)
+    return psds
 
-# Define the CNN model
-class EEGNet(nn.Module):
-    def __init__(self):
-        super(EEGNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=(n_channels, 1), stride=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=(1, n_timepoints), stride=1)
-        self.fc1 = nn.Linear(32, 32)
-        self.fc2 = nn.Linear(32, 2)
+# Classification
+def train_classifier(features, labels):
+    # Use Support Vector Machine as the classifier
+    clf = svm.SVC()
 
-    def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = torch.relu(self.conv2(x))
-        x = torch.max_pool2d(x, (1, 4))
-        x = torch.flatten(x, 1)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+    # Train the classifier
+    clf.fit(features, labels)
 
-# Initialize the model, loss function, and optimizer
-model = EEGNet().to('cpu')
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    return clf
 
-# Training loop
-n_epochs = 100
-for epoch in range(n_epochs):
-    for inputs, labels in train_loader:
-        inputs, labels = inputs.to('cpu'), labels.to('cpu')
+# Main Program
+def main():
+    # Acquire and preprocess the signal
+    raw = acquire_signal('sample_data.fif')
+    raw = preprocess_signal(raw)
 
-        # Zero the parameter gradients
-        optimizer.zero_grad()
+    # Extract features
+    features = extract_features(raw)
 
-        # Forward pass
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+    # Assume we have labels (This part is much more complex in a real scenario)
+    labels = np.random.randint(2, size=len(features))
 
-        # Backward pass and optimization
-        loss.backward()
-        optimizer.step()
+    # Split the data into training set and test set
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2)
 
-    # Print loss every 10 epochs
-    if epoch % 10 == 0:
-        print(f'Epoch {epoch}/{n_epochs}, Loss: {loss.item()}')
+    # Train the classifier
+    clf = train_classifier(X_train, y_train)
 
-# Evaluate the model
-model.eval()
-correct = 0
-total = 0
-with torch.no_grad():
-    for inputs, labels in test_loader:
-        inputs, labels = inputs.to('cpu'), labels.to('cpu')
+    # Test the classifier
+    score = clf.score(X_test, y_test)
 
-        outputs = model(inputs)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    print(f'The accuracy of the classifier is {score*100:.2f}%')
 
-print(f'Test Accuracy: {100 * correct / total}%')
+if __name__ == '__main__':
+    main()
+
